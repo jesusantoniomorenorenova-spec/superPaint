68082264a30f9e8c4ec67c6d5bc36024a937abe2
tono <jesusantoniomorenorenova@gmail.com>
2026-02-11 10:56:30 -0600

    Add files via upload

---
+++ GitHub/superpaint-main/canvas.py
@@ -0,0 +1,111 @@
+from PyQt6.QtWidgets import QWidget, QMessageBox
+from PyQt6.QtCore import Qt, QPoint, QPointF, QRectF
+from PyQt6.QtGui import QPainter, QPen, QImage
+from PyQt6.QtGui import QColor
+from PyQt6.QtWidgets import QFileDialog
+
+class Canvas(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setAttribute(Qt.WidgetAttribute.WA_StaticContents)
+        self.image = QImage(self.size(), QImage.Format.Format_ARGB32)
+        self.image.fill(QColor("#fff"))
+        self.drawing = False
+        self.last_point = QPoint()
+        self.pen_color = QColor("#f00")
+        self.pen_width = 2
+    def paintEvent(self, event):
+        with QPainter(self) as painter:
+            painter.drawImage(event.rect(), self.image, event.rect())
+    def resizeEvent(self, event):
+        
+        if self.width() > self.image.width() or self.height() > self.image.height():
+            new_width =max(self.width(), self.image.width())
+            max_height = max(self.height(), self.image.height())
+            new_image = QImage(new_width, max_height, QImage.Format.Format_ARGB32)
+            new_image.fill(QColor("#2d2d2d"))
+            with QPainter(new_image) as painter:
+                painter.drawImage(0,0,self.image)
+            self.image = new_image
+        super().resizeEvent(event)
+        #self.draw_examples()
+       
+    def draw_examples(self):
+        with QPainter(self.image) as painter:
+            painter.setPen(QPen(QColor("#ff00000"),10,Qt.PenStyle.SolidLine,Qt.PenCapStyle.RoundCap,Qt.PenJoinStyle.RoundJoin))
+            painter.drawLine(300,0,300,600)
+            painter.drawLine(0,300,600,300)
+            painter.drawRect(265,265,70,70)
+        self.update()
+    def mousePressEvent(self, a0):
+        if a0.button() == Qt.MouseButton.LeftButton:
+            self.last_point = a0.position().toPoint()
+            self.drawing = True
+    def mouseMoveEvent(self, a0):
+        if (a0.buttons() & Qt.MouseButton.LeftButton) and self.drawing:
+            self.draw_line_to(a0.position().toPoint())
+
+    def mouseReleaseEvent(self, a0):
+        if (a0.button() == Qt.MouseButton.LeftButton) and self.drawing:
+            self.draw_line_to(a0.position().toPoint())
+            self.drawing=False
+    def draw_line_to(self, end_point):
+        with QPainter(self.image) as painter:
+            painter.setPen(QPen(self.pen_color,self.pen_width,Qt.PenStyle.SolidLine,Qt.PenCapStyle.RoundCap,Qt.PenJoinStyle.RoundJoin))
+            painter.drawLine(self.last_point, end_point)
+            #painter.drawEllipse(end_point, 30,30)
+        self.update()
+        self.last_point = end_point
+    def clear(self):
+        self.image.fill(QColor("#2d2d2d"))
+        self.update()
+    
+    def save_image(self):
+        file_path = QFileDialog.getSaveFileName(
+            self,
+            "Save image",
+            "",
+            "PNG Files (*.png);;All Files (*)"
+        )
+        print(file_path)
+        if file_path:
+            self.image.save(file_path[0])
+            QMessageBox.information(self, "Super Paint","Imagen guardada correctamente")
+    
+    def open_image(self):
+        file_path = QFileDialog.getOpenFileName(
+            self,
+            "Abrir imagen",
+            "",
+            "PNG Files (*.png);;All Files (*)"
+        )
+        if  file_path:
+            self.image = QImage(file_path[0])
+            self.update()
+    def draw_grid(self, value):
+        with QPainter(self.image) as painter:
+            painter.setPen(QPen(QColor("#ff00000"),1,Qt.PenStyle.SolidLine,Qt.PenCapStyle.RoundCap,Qt.PenJoinStyle.RoundJoin))
+            w = self.image.width()
+            h = self.image.height()
+            div = int(w / int(value)) #600/10
+            total_l = int(value)
+            self.clear()
+            for x in range(1,total_l):
+                painter.drawLine(div*x,0,div*x,h)
+                painter.drawLine(0,div*x,w,div*x)
+        self.update()
+    def draw_star(self, value):
+        with QPainter(self.image) as painter:
+            painter.setPen(QPen(QColor("#ff00000"),1,Qt.PenStyle.SolidLine,Qt.PenCapStyle.RoundCap,Qt.PenJoinStyle.RoundJoin))
+            w = self.image.width()
+            h = self.image.height()
+            mid_w = w // 2
+            div = int(mid_w/ int(value)) #600/10
+            self.clear()
+           
+            mid_h = h // 2
+            painter.drawLine(mid_h,0,mid_w,h )
+            painter.drawLine(0,mid_h,w,mid_h)
+            for x in range(0,div):
+                painter.drawLine(mid_w,div*x, (mid_w+ (div*x )),mid_h )
+        self.update()
---
+++ GitHub/superpaint-main/controller.py
@@ -0,0 +1,44 @@
+
+from PyQt6.QtGui import QColor
+
+class MainController:
+    def __init__(self, main_window, ui):
+        self.main_window=main_window
+        self.ui=ui
+        self.canvas = ui.widget
+        self.ui.comboDibujos.addItems(["","Cuadrícula","Estrella"])
+        self.connect_signals()
+    
+    #declaramos los eventos
+    def connect_signals(self):
+        self.ui.txtColor.textChanged.connect(self.update_color)
+        self.ui.slider.valueChanged.connect(self.update_pincel)
+        self.ui.btnBorrador.clicked.connect(self.set_eraser)
+        self.ui.btnGuardar.clicked.connect(self.canvas.save_image)
+        self.ui.actionOpen.triggered.connect(self.canvas.open_image)
+    
+    def set_eraser(self):
+        self.canvas.pen_color = QColor("#2d2d2d")
+   
+    def update_pincel(self,width):
+        figura = self.ui.comboDibujos.currentText()
+        if figura == "":
+            self.canvas.pen_width =width
+        elif figura == "Cuadrícula":
+            self.canvas.draw_grid(width)
+        elif figura == "Estrella":
+            self.canvas.draw_star(width)
+        else:
+            print("Sin selección")
+
+    def update_color(self):
+        color = self.ui.txtColor.toPlainText().strip()
+        print(color)
+        fondo  = QColor(color)
+        self.canvas.pen_color = fondo
+        if fondo.isValid():
+            self.ui.txtColor.setStyleSheet(f"background-color: {color};color:{self.color_inverse(fondo).name()}")
+    #F => 255
+    def color_inverse(self, color):
+        inverse = QColor(255-color.red(), 255-color.green(), 255-color.blue())
+        return inverse
